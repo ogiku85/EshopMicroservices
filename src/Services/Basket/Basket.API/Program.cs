e8c08a5335a30a4f1928d0665cce3d5e0165309f
Osamuyimen Ogiku <ogiku85@yahoo.co.uk>
2025-07-07 15:57:33 +0200

    Add missing files

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,79 @@
+
+using BuildingBlocks.Exceptions.Handler;
+using Discount.Grpc;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+var assembly = typeof(Program).Assembly;
+
+// Application Services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+// Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.Username);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRespository>();
+
+/*
+builder.Services.AddScoped<IBasketRepository>(provider =>
+{
+    var basketRepository = provider.GetRequiredService<IBasketRepository>();
+    return new CachedBasketRespository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+});
+*/
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+// Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
+});
+
+
+// Cross Cutting Concerns
+
+//Register Exception handler
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
+var app = builder.Build();
+
+// configure the HTTP request pipeline
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
